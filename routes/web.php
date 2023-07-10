1745d37a5b3fefad8144ca81a788d2665c0f3ac4
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-07-10 14:28:12 +0100

    add hello world with Blade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Hello World';
+
+    $title = 'Hello World';
+
+    return view('home', compact('title'));
 });
